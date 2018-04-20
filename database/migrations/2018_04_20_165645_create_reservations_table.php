10657e845f97eaeac7dba4393d52273e6329c3c8
deenaahmed <deenaahmedkamal8@gmail.com>
2018-04-20 19:13:04 +0200

    Adding tables

---
+++ database/migrations/2018_04_20_165645_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('clientid'); //foreign key for the client table 
+            $table->integer('roomnumber'); //foreignkey for the room table
+            $table->integer('status'); //0:binding 1:approved 
+            $table->integer('clientpaidprice');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
