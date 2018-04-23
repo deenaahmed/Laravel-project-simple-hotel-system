cb858a5ce2bc140feb4242bf7f397cd1d91a9c34
deenaahmed <deenaahmedkamal8@gmail.com>
2018-04-23 18:58:25 +0200

    database edited

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,16 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('mobile')->nullable(); //for client
+            $table->string('country')->nullable(); // for client
+            $table->string('gender')->nullable(); /// for client (male,female) 
+            $table->string('national_id')->nullable();  //for manager and recep
+            $table->string('avatar_image')->nullable(); //for manager and recep
+            // $table->string('admin_creator')->nullable(); //for manager 
+            // $table->string('recep_creator')->nullable(); //for client
+            // $table->string('manager_creator')->nullable(); //for recep
+            $table->string('creator')->nullable(); // holds the id of the creator
+            $table->integer('type'); //admin,manager,receptionist,client 1,2,3,4
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2018_04_20_161734_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('number');
+            $table->integer('capacity');
+            $table->string('createdby'); ///foriegn key to the manager table 
+            $table->integer('floor_id'); // foreign key to the floor table 
+            $table->string('isavailable'); //true or false 
+            $table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
