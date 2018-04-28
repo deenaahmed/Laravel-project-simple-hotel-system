b9479e78520b30b395c7c56fef1c6957c2c06c36
deenaahmed <deenaahmedkamal8@gmail.com>
2018-04-28 21:54:08 +0200

    merge #7

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -132,10 +132,10 @@
             @endhasrole
             @hasanyrole('admin|manager')
             <li><a href="receptionists"><i class="fa fa-circle-o"></i> Manage Receptionists</a></li>
-            @endhasrole
-            <li><a href="clients"><i class="fa fa-circle-o"></i> Manage Clients</a></li>
             <li><a href="rooms"><i class="fa fa-circle-o"></i> Manage Rooms</a></li>
             <li><a href="floors"><i class="fa fa-circle-o"></i> Manage Floors</a></li>
+            @endhasrole
+            <li><a href="clients"><i class="fa fa-circle-o"></i> Manage Clients</a></li>
           </ul>|
         </li>
         <li class="treeview">
